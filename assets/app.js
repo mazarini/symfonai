3ee0fa876200b1dfa8a45f0afc4b44003e386be6
Mazarini <mazarini@protonmail.com>
2025-10-24 12:57:01 +0200

    feat: add Bootstrap and configure Webpack Encore
    - Install Bootstrap via npm
    - Create app.scss to include Bootstrap and chat-specific styles
    - Include app.js to compile assets
    - Update base.html.twig to load compiled CSS and JS

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// Import du CSS compilé depuis Sass
+import './styles/app.scss';
+
+// Import JS de Bootstrap
+import 'bootstrap';
