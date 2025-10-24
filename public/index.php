75bf2c236b2f872ed6ac52858f890656a0e0678f
Mazarini <mazarini@protonmail.com>
2025-10-24 11:19:27 +0200

    Add AI controller, chat Twig template, and related front-end packages

--- public/index.php
+++ public/index.php
@@ -1,8 +1,28 @@
 <?php
 
-use App\Kernel;
+declare(strict_types=1);
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+/*
+ * Copyright (C) 2025 Mazarini <mazarini@pm.me>.
+ * This file is part of mazarini/symfonai.
+ *
+ * mazarini/symfonai is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * mazarini/symfonai is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mazarini/symfonai. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use Mazarini\SymfonAI\Kernel;
+
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * Copyright (C) 2025 Mazarini <mazarini@pm.me>.
+ * This file is part of mazarini/symfonai.
+ *
+ * mazarini/symfonai is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * mazarini/symfonai is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mazarini/symfonai. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 
-if ($_SERVER['APP_DEBUG']) {
+if ($_SERVER['APP_DEBUG'] === '1' || $_SERVER['APP_DEBUG'] === true) {
     umask(0000);
 }
